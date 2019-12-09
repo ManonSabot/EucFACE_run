4cfe705a618a66633cae8f8249fb58ad06e2e0a4
Mengyuan Mu <bibi_king@163.com>
2019-12-09 22:10:50 +1100

    fix bugs, and sen_exp can work now

--- run_sen_exp.py
+++ run_sen_exp.py
@@ -6,26 +6,30 @@ __developer__ = "MU Mengyuan"
 import os
 import sys
 import glob
+import shutil
 import pandas as pd
 import numpy as np
 import netCDF4 as nc
 import datetime
-from scipy.interpolate import interp1d
-from scipy.interpolate import griddata
-import cable_run_sen_exp
+from cable_run_sen_exp import RunCable
 
 def main(sen_para, sen_value, operator, met_dir, met_subset):
     if (not os.path.exists(met_dir)):
         raise Exception("No met folder: %s" %met_dir)
     else:
-        met_dir_new = alter_met_parameter(sen_para, sen_value, operator, met_dir, met_subset):
-     run_cable(met_dir_new, met_subset)
+        print("we are changing met file")
+        met_dir_new = alter_met_parameter(sen_para, sen_value, operator, met_dir, met_subset)
+
+    print("we are running cable")
+    run_cable(met_dir_new, met_subset)
 
 def alter_met_parameter(sen_para, sen_value, operator, met_dir, met_subset):
 
-    met_dir_new = os.path.join(met_dir, "_%s%s%s" %( sen_para, operator ,sen_value.replace('.', '') ))
+    met_dir_new = "%s_%s%s%s" %(met_dir, sen_para, operator ,sen_value.replace('.', '') )
 
     if len(met_subset) == 0:
+        if os.path.exists(met_dir_new):
+            shutil. rmtree(met_dir_new)
         shutil.copytree(met_dir, met_dir_new)
         met_files = glob.glob(os.path.join(met_dir_new, "*.nc"))
     else:
@@ -35,23 +39,23 @@ def alter_met_parameter(sen_para, sen_value, operator, met_dir, met_subset):
             shutil.copy(os.path.join(met_dir, i) , met_dir_new)
         met_files = glob.glob(os.path.join(met_dir_new, "*.nc"))
     for met_file in met_files:
-        f = nc.Dataset(met_file, 'w', format='NETCDF4')
+        f = nc.Dataset(met_file, 'r+', format='NETCDF4')
         f.parameter_sensitivity = 'alter %s as %s%s%s' % (sen_para, sen_para, operator, sen_value)
 
         sen_para_vec = "%s_vec" %sen_para
-
         if operator == "x":
-            f.variables[sen_para][:]         = f.variables[sen_para][:] * sen_value
-            f.variables[sen_para_vec][:,0,0] = f.variables[sen_para_vec][:,0,0] * sen_value
+            print(sen_para)
+            f.variables[sen_para][:,:]       = f.variables[sen_para][:,:] * float(sen_value)
+            f.variables[sen_para_vec][:,0,0] = f.variables[sen_para_vec][:,0,0] * float(sen_value)
         elif operator == "=":
-            f.variables[sen_para][:]         = sen_value
-            f.variables[sen_para_vec][:,0,0] = sen_value
+            f.variables[sen_para][:,:]       = float(sen_value)
+            f.variables[sen_para_vec][:,0,0] = float(sen_value)
         elif operator == "+":
-            f.variables[sen_para][:]         = f.variables[sen_para][:,0,0] + sen_value
-            f.variables[sen_para_vec][:,0,0] = f.variables[sen_para_vec][:,0,0] + sen_value
+            f.variables[sen_para][:,:]       = f.variables[sen_para][:,:] + float(sen_value)
+            f.variables[sen_para_vec][:,0,0] = f.variables[sen_para_vec][:,0,0] + float(sen_value)
         elif operator == "-":
-            f.variables[sen_para][:]         = f.variables[sen_para][:] - sen_value
-            f.variables[sen_para_vec][:,0,0] = f.variables[sen_para_vec][:,0,0] - sen_value
+            f.variables[sen_para][:,:]       = f.variables[sen_para][:,:] - float(sen_value)
+            f.variables[sen_para_vec][:,0,0] = f.variables[sen_para_vec][:,0,0] - float(sen_value)
         f.close()
     return met_dir_new
 
@@ -62,10 +66,10 @@ def run_cable(met_dir_new, met_subset):
     cable_exe = "cable_met_HDM_31uni"
     #------------- Change stuff ------------- #
     met_dir = met_dir_new
-    log_dir = "/srv/ccrc/data25/z5218916/cable/EucFACE/EucFACE_run/logs/%s_%s" % ( met_fname, case_name )
-    output_dir = "/srv/ccrc/data25/z5218916/cable/EucFACE/EucFACE_run/outputs/%s_%s" % ( met_fname, case_name )
-    restart_dir = "/srv/ccrc/data25/z5218916/cable/EucFACE/EucFACE_run/restart_files/%s_%s" % ( met_fname, case_name )
-    namelist_dir = "/srv/ccrc/data25/z5218916/cable/EucFACE/EucFACE_run/namelists/%s_%s" % ( met_fname, case_name )
+    log_dir = "/srv/ccrc/data25/z5218916/cable/EucFACE/EucFACE_run_sen/logs/%s_%s" % ( met_fname, case_name )
+    output_dir = "/srv/ccrc/data25/z5218916/cable/EucFACE/EucFACE_run_sen/outputs/%s_%s" % ( met_fname, case_name )
+    restart_dir = "/srv/ccrc/data25/z5218916/cable/EucFACE/EucFACE_run_sen/restart_files/%s_%s" % ( met_fname, case_name )
+    namelist_dir = "/srv/ccrc/data25/z5218916/cable/EucFACE/EucFACE_run_sen/namelists/%s_%s" % ( met_fname, case_name )
     aux_dir = "/srv/ccrc/data25/z5218916/cable/src/CABLE-AUX/"
     cable_src = "/srv/ccrc/data25/z5218916/cable/src/Marks_latest_branch_with_fixes/Marks_latest_branch_with_fixes_gw_for_EucFace_fix_met_multi-layer/"
     mpi = False
@@ -86,9 +90,10 @@ if __name__ == "__main__":
     sen_values = ["0.01","0.1","10.","100."]
 
     met_case   = "met_LAI_vrt_SM_swilt-watr_31uni"
-    met_dir    = "/srv/ccrc/data25/z5218916/cable/EucFACE/EucFACE_run/met/%s" %met_case
+    met_dir    = "/srv/ccrc/data25/z5218916/cable/EucFACE/EucFACE_run_sen/met/%s" %met_case
     met_subset = []
-
+    print("we start!")
     for sen_value in sen_values:
-        main(sen_para = sen_para, sen_values = sen_value,\
+        print("we are trying %s%s%s" %(sen_para,operator,sen_value))
+        main(sen_para = sen_para, sen_value = sen_value,\
              operator=operator, met_dir = met_dir, met_subset = met_subset )
