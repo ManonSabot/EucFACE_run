67660fb49fb984beef02da9a330c02da89dd7174
Mengyuan Mu <bibi_king@163.com>
2020-04-16 00:36:09 +1000

    Run add gamma to restart file in order to run Hvrd's plant water stress function

---
+++ change_restart_file.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""
+Run add gamma to restart file in order to run Hvrd plant water stress function
+"""
+
+__author__    = "MU Mengyuan"
+
+import os
+import sys
+import glob
+import pandas as pd
+import numpy as np
+import netCDF4 as nc
+import datetime
+from scipy.interpolate import interp1d
+from scipy.interpolate import griddata
+from scipy.signal import savgol_filter
+
+def main(restart_fname):
+
+    # create file and write global attributes
+    f = nc.Dataset(restart_fname, 'w', format='NETCDF4')
+
+    gamma = f.createVariable('gamma', 'f4', ('mp',))
+    gamma.units = "-"
+    gamma.long_name = "Parameter in root efficiency function (Lai and Katul 2000)"
+    gamma._FillValue = -1.e+33
+    gamma.missing_value = -1.e+33
+    gamma[:] = 0.03
+
+    f.close()
+
+if __name__ == "__main__":
+
+    restart_fname = "/srv/ccrc/data25/z5218916/cable/EucFACE/EucFACE_run_31layers/restart_files/met_LAI_vrt_swilt-watr-ssat_SM_31uni_2013-2018-1-17_litter_Hvrd/EucFACE_amb_restart.nc"
+
+    main(restart_fname)
